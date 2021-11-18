da1d8c9f3e1f79f1a48f2220a0883c49392e6232
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-11-18 21:50:37 +0100

    feat: use clap and add readme
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Joule
+
+This tool was made for monitoring the energy consumption of command, the same way the command `time` monitors the time used by a command.
+
+## Howto use it
+
+```bash
+# clone the repository
+git clone https://github.com/jdrouet/joule
+# go into the repository folder
+cd joule
+# build it
+cargo build --release
+# use it
+sudo ./target/release/joule make real-music
+```
+
+You need to be a super user to execute that command, to access the powercap files.
+
+## More information
+
+⚠️ This package has nothing related to any French person ⚠️ 
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,11 @@ mod process;
 mod record;
 mod snapshot;
 
-use std::env::args;
-use std::process::{Command, Stdio};
-
 fn main() {
-    let config = config::Config::default();
+    let config = config::Config::parse();
     let snapshot_reader = config.snapshot_reader();
-    let mut args = args()
-        .enumerate()
-        .filter(|(index, _)| *index > 0)
-        .map(|(_, value)| value)
-        .collect::<Vec<String>>();
-    println!("executing {:?}", args);
-    let cmd = args.remove(0);
     let before = snapshot_reader.read().unwrap();
-    Command::new(cmd)
-        .args(args)
-        .stdout(Stdio::inherit())
-        .output()
-        .expect("failed to execute command...");
+    config.execute();
     let after = snapshot_reader.read().unwrap();
     before.print();
     after.print();
